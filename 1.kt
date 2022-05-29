4a0da9de7ea601f7f97a4df1bb5110e27a8a1dc4
DjairMaynart <106343149+DjairMaynart@users.noreply.github.com>
2022-05-28 21:14:03 -0300

    Update 1.kt

--- 1.kt
+++ 1.kt
@@ -8,12 +8,44 @@ import io.ktor.response.*
 import io.ktor.features.*
 import io.ktor.html.*
 import io.ktor.request.*
+import java.io.*
+import kotlin.io.*
+
+
+class App {
+    val greeting: String
+        get() {
+            return "Hello World!"
+        }
+}
 
 fun main() {
+
     embeddedServer(Netty, port = 7654){
         routing{
-            get("/teste.text"){
-                call.respondText("testando")
+                get("/"){
+            
+                call.respondText("""
+                
+                <html>
+                <head>
+                    <title>Minha 1ª página html</title> 
+                </head>
+
+                 <input type="text" size="100" placeholder="Insira seu texto" name="texto">
+                 <input type="button" value="Enviar" name="botao"><br><br>
+                 <button>ola</button>
+                 </form>
+
+                </html>
+
+                
+                                
+                """,ContentType.Text.Html)
+                val x = call.parameters["texto"]
+            }
+            static("/estatico/"){
+                files("static/")
             }
         }
     }.start(wait=true)
